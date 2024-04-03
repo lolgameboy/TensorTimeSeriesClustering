deea480f5ae413098b60dc25bd244d1c5ee5f3b8
Yukekbyte <lowie.debois@gmail.com>
2024-04-03 12:29:31 +0200

    notebood + figs changes

--- scripts/figs.py
+++ scripts/figs.py
@@ -73,6 +73,8 @@ def plot_rel_err(ranks, max_approxs, colors, add_matrix_aca_t=False, repeat=50, 
 
     if len(colors) != len(max_approxs):
         raise Exception("Amount of colors not right.")
+    if repeat < 2:
+        raise Exception("Repeat needs to be 2 or higher to calculate standard deviations.")
 
     try:
         data = np.load(f'saved_fig_data/data(err,{ranks},{max_approxs},{add_matrix_aca_t},{repeat}).npy')
@@ -86,14 +88,19 @@ def plot_rel_err(ranks, max_approxs, colors, add_matrix_aca_t=False, repeat=50, 
     fig, ax = plt.subplots()
 
     if ptype == 'bar':
+        if len(ranks) < 2:
+            raise Exception("At least 2 ranks needed for a bar plot.")
         n = len(data)
-        if n == 1:
-            width = 4
-        elif n == 2:
-            width = 2.2
-        else:
-            width = 1.5
-        fig.set_size_inches(max(0, 2*(n-1)) + 6.4, 4.8, forward=True) # many bars (large n) needs wider plot
+
+         # width is based on highest rank and inter rank distance (assumes uniform inter-rank distance: e.g. [5, 10, 15, 20] and NOT [5, 9, 17, 20])
+        delta = ranks[-1] - ranks[-2]
+        total = ranks[-1]
+        alpha = 10 * delta/total
+        width = (delta - alpha)/n
+
+        # fig size is based on amount of bars (n)
+        fig.set_size_inches(3 + 6.4, 4.8, forward=True) # many bars (large n) needs wider plot
+
         for i, d in enumerate(data):
             avgs = list(map(stat.mean, d))
             stdevs = list(map(stat.stdev, d))
@@ -257,6 +264,8 @@ def plot_rel_err_vs_rel_dtw(ranks, max_approxs, colors, add_matrix_aca_t=False, 
 
     if len(colors) != len(max_approxs):
         raise Exception("Amount of colors not right.")
+    if repeat < 2:
+        raise Exception("Repeat needs to be 2 or higher to calculate standard deviations.")
 
     try:
         err_data = np.load(f'saved_fig_data/data(err,{ranks},{max_approxs},{add_matrix_aca_t},{repeat}).npy')
@@ -283,7 +292,7 @@ def plot_rel_err_vs_rel_dtw(ranks, max_approxs, colors, add_matrix_aca_t=False, 
         raise Exception('Plot type not recognized.')
 
     # Styling of the plot
-
+    fig.set_size_inches(2 + 6.4, 4.8, forward=True)
         # x and y axis
     plt.xlabel('Relatieve % DTW operaties')
     plt.xticks(list(map(lambda x: round(x, 1), count_data[-1])), fontsize=15)
@@ -327,8 +336,16 @@ def plot_rel_err_vs_rel_dtw(ranks, max_approxs, colors, add_matrix_aca_t=False, 
     plt.savefig(f'figures/rel_fout_rel_dtw{str(tuple(max_approxs)).replace(" ", "")}(rpt{repeat})(rnk{ranks[-1]}).svg', transparent=True, bbox_inches=0)
     plt.show()
 
-colors = ['firebrick', 'indigo', 'teal', 'greenyellow', 'violet']
+# type     1            2         3              5         8       10/20
+colors = ['firebrick', 'indigo', 'greenyellow', 'violet', 'teal', 'indigo']
 
-#plot_rel_err(range(5, 51, 10), [1, 3, 8, 20], ['firebrick', 'greenyellow', 'teal', 'indigo'], add_matrix_aca_t=True, repeat=50, ptype='bar')
+#plot_rel_err(range(5, 51, 5), [2], ['indigo'], add_matrix_aca_t=False, repeat=50)
+#plot_rel_err(range(5, 41, 5), [3], ['greenyellow'], add_matrix_aca_t=False, repeat=50)
+#plot_rel_err(range(5, 31, 3), [5], ['violet'], add_matrix_aca_t=False, repeat=50)
+#plot_rel_err(range(5, 101, 10), [5], ['violet'], add_matrix_aca_t=False, repeat=2)
+
+
+plot_rel_err(range(2, 15, 2), [1, 2, 3, 5], ['firebrick', 'indigo', 'greenyellow', 'violet'], add_matrix_aca_t=False, repeat=50)
+#plot_rel_err(range(2, 15, 2), [1, 3, 5, 8, 10], ['firebrick', 'greenyellow', 'violet', 'teal', 'indigo'], add_matrix_aca_t=False, repeat=20)
 #plot_rel_err_vs_rel_dtw(range(5, 51, 10), [1, 8], ['firebrick', 'teal'], add_matrix_aca_t=True, repeat=3, ptype='line')
-plot_rel_err(range(5,51,5), [1], ['firebrick'], add_matrix_aca_t=True)
+#plot_rel_err(range(5,51,5), [1], ['firebrick'], add_matrix_aca_t=True)
